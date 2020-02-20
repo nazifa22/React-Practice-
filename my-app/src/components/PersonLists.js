5f0e1c0c0e273f22bd9b7fd11f63446819d12605
Nazifa Rashid <nazifarashid1822@gmail.com>
2020-02-20 18:14:14 +0600

    Added key

--- my-app/src/components/PersonLists.js
+++ my-app/src/components/PersonLists.js
@@ -24,7 +24,7 @@ const PersonLists = () => {
     ]
     
     const personLists = persons.map(person => (
-        <List person = {person} />
+        <List key = {person.id} person = {person} />
     ))
     
     return (
