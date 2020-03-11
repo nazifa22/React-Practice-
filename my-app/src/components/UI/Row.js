e4b42ff8e0f7d5e4d4969aaa6ede4e80de2cb8fc
Nazifa Rashid <nazifarashid1822@gmail.com>
2020-03-11 19:02:14 +0600

    Addedf UI component

---
+++ my-app/src/components/UI/Row.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Row.css';
+
+function Row(props) {
+    let align;
+    if(props.align == 'center') {
+        align = 'align-center'
+    }
+    else if(props.align == 'bottom') {
+        align = 'align-bottom'
+    }
+    if(props.align == 'top') {
+        align = 'align-top'
+    }
+    
+    return (
+        <div className = {`row ${align}`}></div>
+    )
+}
+
+export default Row;
